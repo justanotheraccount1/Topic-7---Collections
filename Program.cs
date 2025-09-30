b4e83f48cae469416ba2564b5bde35ebdb749a78
339729139 <339729139@gotvdsb.ca>
2025-09-30 08:39:21 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Topic_7___Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random generator = new Random();
+            int[] a = new int[29];
+            for(int i = 0; i < a.Length; i++)
+            {
+                a[i] = generator.Next(1, 101);
+                Console.Write($"{a[i]}, ");
+            }
+        }
+    }
+}
